a1d5b2842d8578355bd753cc9c3d0f9ac1d21ac5
an2niy85 <an2niy85@mail.ru>
2024-03-19 10:48:59 +0400

    Add project files.

---
+++ MyMessager-Stepic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMessager_Stepic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Начало проекта");
+        }
+    }
+}
