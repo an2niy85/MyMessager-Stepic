6104c30c2f16a4f23af5d0ff22c43c60c2af3075
an2niy85 <an2niy85@mail.ru>
2024-03-19 11:14:59 +0400

    Подключил NewtonJSON

--- MyMessager-Stepic/Program.cs
+++ MyMessager-Stepic/Program.cs
@@ -1,12 +1,22 @@
-﻿namespace MyMessager_Stepic
+﻿using Newtonsoft.Json;
+
+namespace MyMessager_Stepic
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Начало проекта");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializeMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializeMsg);
+
+            //{ "UserName":"RusAl","MessageText":"Privet","TimeStamp":"2024-03-19T07:13:40.7266465Z"}
+            //RusAl < 19.03.2024 7:13:40 >: Privet
+
+            //Console.WriteLine("Начало проекта");
+            //Console.WriteLine(msg.ToString());
         }
     }
 }
